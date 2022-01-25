65d010e5e04e893a85cf63f9334a0ca64441fe03
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2022-01-25 10:56:42 +0100

    export types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+export * from "./types";
+
 export * as statemine from "./chains/statemine";
 export * as moonriver from "./chains/moonriver";
 export * as karura from "./chains/statemine";
--- src/types/chain-balance-interface.ts
+++
@@ -1,7 +0,0 @@
-import { Observable } from "rxjs";
-import { Balance } from "./balance";
-
-export interface ChainBalance<C> {
-  provideContext: (context: C) => void;
-  balance$: (address: string) => Observable<Balance>;
-}
