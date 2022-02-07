3b8087e2b1ad3af728ccac41cbeb2f03078a2c3c
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2022-02-07 10:09:03 +0100

    use samve rxjs and polkadot api versions as marketplaces.

--- src/types/network-balance.ts
+++ src/types/network-balance.ts
@@ -12,7 +12,7 @@ import { Balance } from ".";
 import * as A from "./address";
 
 export type NetworkBalance<C, AT extends A.Type> = {
-  provideContext: (context: C | Promise<C>) => Promise<void>;
+  provideContext: (context: C | Promise<C>) => Promise<NetworkBalance<C, AT>>;
   balance$: (address: string) => Observable<Balance>;
   balance: (address: string) => Promise<Balance>;
 };
@@ -33,44 +33,42 @@ export const implement = <C, AT extends A.Type>(
 ): NetworkBalance<C, AT> => {
   const context$ = new ReplaySubject<C>();
 
-  const provideContext = async (context: C | Promise<C>) =>
-    context$.next(await context);
-
-  const balance$ = (address: string) => {
-    return combineLatest([context$, of(A.wrap(address))]).pipe(
-      mergeMap(([context, address]) => {
-        if (A.FormatError.is(address)) return throwError(() => address);
-
-        if (!A.is(impl.addressType, address))
-          return throwError(
-            () =>
-              new A.FormatError(`Expected address of type ${impl.addressType}`)
-          );
-
-        return fromEventPattern<Balance>(
-          (cb) => {
-            return impl.stream(context, address.raw, (value) => {
-              cb(impl.decode(value));
-            });
-          },
-          (_, unsubscribe: Promise<() => void>) => {
-            unsubscribe.then((u) => u());
-          }
-        );
-      })
-    );
-  };
-
-  const balance = async (address: string) => {
-    A.assert(impl.addressType, address);
-    const api = await firstValueFrom(context$);
-    const value = await impl.read(api, address);
-    return impl.decode(value);
-  };
-
   return {
-    provideContext,
-    balance$,
-    balance,
+    async provideContext(context: C | Promise<C>) {
+      context$.next(await context);
+      return this;
+    },
+    async balance(address: string) {
+      A.assert(impl.addressType, address);
+      const api = await firstValueFrom(context$);
+      const value = await impl.read(api, address);
+      return impl.decode(value);
+    },
+    balance$(address: string) {
+      return combineLatest([context$, of(A.wrap(address))]).pipe(
+        mergeMap(([context, address]) => {
+          if (A.FormatError.is(address)) return throwError(() => address);
+
+          if (!A.is(impl.addressType, address))
+            return throwError(
+              () =>
+                new A.FormatError(
+                  `Expected address of type ${impl.addressType}`
+                )
+            );
+
+          return fromEventPattern<Balance>(
+            (cb) => {
+              return impl.stream(context, address.raw, (value) => {
+                cb(impl.decode(value));
+              });
+            },
+            (_, unsubscribe: Promise<() => void>) => {
+              unsubscribe.then((u) => u());
+            }
+          );
+        })
+      );
+    },
   };
 };
